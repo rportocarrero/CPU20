e47d0152f078d284486a1ddd571a1c54c4061b89
Ryan Portocarrero <portocarrero.ryan@gmail.com>
2022-12-15 23:47:01 -0500

    Update tb_registers.sv
    Modified testbench to write test results to log file.

--- test/tb_registers.sv
+++ test/tb_registers.sv
@@ -5,7 +5,7 @@ module tb_registers;
     reg[3:0] r_addr;
     wire[15:0] w_data;
     reg r_rw;
-    integer fd, r;
+    integer fd, r, fo;
 
     reg[15:0] vec_input;
     reg[15:0] vec_exp;
@@ -16,10 +16,19 @@ module tb_registers;
     begin
         $display("Testing Register File...");
 
+
         fd = $fopen("test/vectors/tb_registers_vec.csv", "r");
-        if(fd) $display("opened test vector file %0d", fd);
+        if(fd) $display("opened test vector file %h", fd);
         else begin
-            $display("unable to open test vector file %0d", fd);
+            $display("unable to open test vector file %h", fd);
+            $finish;
+        end
+
+
+        fo = $fopen("test/logs/tb_registers.txt", "w");
+        if(fo) $display("opened test output file %h", fo);
+        else begin
+            $display("unable to open output text file %h", fo);
             $finish;
         end
 
@@ -29,11 +38,10 @@ module tb_registers;
             r_addr = vec_input[3:0];
             r_rw = vec_input[4];
             #20;
-            if(w_data !== vec_exp) begin
-                $display("address: %h, r/w: %b, data:%h, expected:%h", r_addr, r_rw, w_data,vec_exp);
-            end
+            $fwrite(fo, "address: %h, r/w: %b, data:%h, expected:%h\n", r_addr, r_rw, w_data,vec_exp);
         end
         $fclose(fd);
+        $fclose(fo);
         $finish;
     end
 
